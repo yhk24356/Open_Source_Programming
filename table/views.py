5a624f9dc6fd411ca113ecde38be8f0f12a1ad5d
yhk24356 <sangmin9101@naver.com>
2022-06-04 12:58:28 +0900

    브랜치 정리

--- table/views.py
+++ table/views.py
@@ -4,4 +4,4 @@ from rest_framework.views import APIView
 
 class Main(APIView):
     def get(self, request):
-        return render(request, "table.html")
+        return render(request, "table2.html")
