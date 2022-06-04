690330ed3eb854a6fa52e76c7a29496b8cbe2977
unknown <yukmj1123@naver.com>
2022-06-04 22:06:53 +0900

    오류 수정 및 합침

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 
 def main(request):
     return render(request, 'main.html')
+
+def cold_medicine(request):
+    return render(request, 'cold_medicine.html')
+
+def back_pain_medicine(request):
+    return render(request, 'back_pain_medicine.html')
+
+def headache_medicine(request):
+    return render(request, 'headache_medicine.html')
+
+def muscle_pain_medicine(request):
+    return render(request, 'muscle_pain_medicine.html')
+
+def stomachache_medicine(request):
+    return render(request, 'stomachache_medicine.html')
+
+def toothache_medicine(request):
+    return render(request, 'toothache_medicine.html')
